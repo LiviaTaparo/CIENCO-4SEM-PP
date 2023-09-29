8d9f5be3bf7a0b992f5f92d2645a5298d5142b9f
Lívia de Oliveira Táparo <125320957+LiviaTaparo@users.noreply.github.com>
2023-09-28 21:12:26 -0300

    Add files via upload

--- Exercicio_22_Lista/src/Main.java
+++ Exercicio_22_Lista/src/Main.java
@@ -2,6 +2,16 @@
 //de 5, no intervalo de 1 a 500.
 public class Main {
     public static void main(String[] args) {
-        
+
+        int num, i;
+
+        for(i = 1; i <= 500; i++)
+        {
+            if(i % 5 == 0){
+                System.out.println("Multiplos de 5: "+ i);
+            }
+
+        }
+
     }
 }
